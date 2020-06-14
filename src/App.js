7318d14696f63251e5a0d2c039ec6b1faef3e199
awis-mbp <awis@awislabs.com>
2020-06-14 09:36:59 +0800

    feat: can display user

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/navbar.component";
+import UserList from "./components/user-list.component";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <br />
+        <div className="container">
+          <div className="auth-wrapper">
+            <div className="auth-inner">
+
+              <Route path="/" exact component={ UserList }/>
+              <Route path="/users" exact component={ UserList }/>
+
+
+
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ src/components/navbar.component.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+
+export default class Navbar extends Component {
+
+
+  render() {
+
+    const storedJwt = localStorage.getItem('token');
+
+    if (storedJwt == null) {
+      return <Redirect to="/login" />
+    }
+
+
+    return (
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">Complete Developer Network</Link>
+        <div className="collpase navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+
+            
+
+            <li className="navbar-item">
+              <Link to="/users" className="nav-link">Users</Link>
+            </li>
+
+            
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
