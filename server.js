38454ca0f19648edfdb9b29e7d1bcf4d4ae06006
awis-mbp <awis@awislabs.com>
2020-06-14 13:57:18 +0800

    update gitignore and update build path

--- server.js
+++ server.js
@@ -12,11 +12,14 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors());
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+
 app.use(express.json());
 // app.use(jwt());
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/')))
+
 
 
 const uri = process.env.MONGODB_URI || 'mongodb://localhost/cdn'
@@ -31,9 +34,11 @@ const usersRouter = require('./routes/users');
 
 app.use('/api/users', usersRouter);
 
+
+
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/public/index.html'))
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
 app.listen(port, () => {
