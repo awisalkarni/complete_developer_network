b10bbb3f830a09e1086a5ec43806b388238d03b5
awis-mbp <awis@awislabs.com>
2020-06-14 13:25:33 +0800

    restructure to deploy to heroku

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const config = require('./helpers/config.json');
+// const jwt = require('./helpers/jwt');
+const mongoose = require('mongoose');
+const path = require('path')
+
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+// app.use(jwt());
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/')))
+
+
+const uri = process.env.MONGODB_URI || 'mongodb://localhost/cdn'
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+const usersRouter = require('./routes/users');
+
+app.use('/users', usersRouter);
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/public/index.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
